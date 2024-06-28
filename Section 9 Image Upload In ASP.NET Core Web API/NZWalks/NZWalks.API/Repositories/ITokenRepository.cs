934e1a9c737523642fa50e1b4952a050f0654758
Bikash Dumrakoti <bikasdumrakoti@gmail.com>
2024-06-28 17:51:13 +0545

    Completed Section 9: Image Upload In ASP.NET Core Web API.

---
+++ Section 9 Image Upload In ASP.NET Core Web API/NZWalks/NZWalks.API/Repositories/ITokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NZWalks.API.Repositories
+{
+    public interface ITokenRepository
+    {
+        string CreateJWT(IdentityUser user, List<string> roles);
+    }
+}
